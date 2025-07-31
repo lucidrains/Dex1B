79822df7a4d9d4e02d113b46326567ace7143480
lucidrains <lucidrains@gmail.com>
2025-07-31 07:41:46 -0700

    just write out the cvae, practice makes perfect

--- Dex1B/Dex1B.py
+++ Dex1B/Dex1B.py
@@ -15,11 +15,68 @@ def default(v, d):
 
 # classes
 
-class VAE(Module):
-    def __init__(self):
+class CVAE(Module):
+    def __init__(
+        self,
+        dim,
+        dim_hiddens = (256, 512, 256), # from Table 6. in paper
+        kl_loss_weight = 1e-4
+    ):
+        super().__init__()
+        assert len(dim_hiddens) > 0
+        dim_latent = default_layer_sizes[-1]
+
+        self.encode = MLP(dim, *default_layer_sizes)
+
+        self.to_mean_log_variance = nn.Linear(dim_latent, dim_latent * 2, bias = False)
+
+        self.decode = MLP(*default_layer_sizes, dim)
+
+        # loss weights
+
+        self.kl_loss_weight = kl_loss_weight
+
+    def forward(
+        self,
+        inp, # (b d)
+        return_loss = False
+    ):
+
+        encoded = self.encode(inp)
+
+        mean, log_variance = self.to_mean_log_variance(encoded).chunk(2, dim = -1)
+
+        std = (0.5 * log_variance).exp()
+
+        noise = torch.randn_like(mean)
+
+        reparamed = mean + std * noise
+
+        recon = self.decode(reparamed)
+
+        if not return_loss:
+            return recon
+
+        mse_loss = F.mse_loss(recon, inp)
+
+        kl_loss = 0.5 * (mean.square() + log_variance.exp() - log_variance - 1.).sum(dim = -1).mean()
+
+        total_loss = (
+            mse_loss +
+            kl_loss * self.kl_loss_weight
+        )
+
+        loss_breakdown = (mse_loss, kl_loss)
+
+        return total_loss, loss_breakdown
+
+class DexSimple(Module):
+    def __init__(
+        self,
+        pointnet: Module,
+        cvae: CVAE
+    ):
         super().__init__()
 
-
-class Dex1B(Module):
-    def __init__(self):
-        super().__init__()
+        self.pointnet = pointnet
+        self.cvae = cvae
